af18f475d633499ecd1dcf22bcdb35fc87c1f61b
Christian V <viray.christianj@gmail.com>
2021-03-10 04:47:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,11 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
@@ -13,7 +18,41 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE name = ? 
+      LIMIT 1 
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
+  
+  def self.all_students_in_grade_9(grade)
+    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+      LIMIT 1 
+    SQL
+    
+    DB[:conn].execute(sql, grade).map do (grade)
+     self.find_by_grade(grade)
+    end 
+  end 
+    
+  end 
+  
+  
+  
+  
+  
+  
+  
   
   def save
     sql = <<-SQL
